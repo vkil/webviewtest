4365431cd70d78e5f83c53aa1aad0e47d4c6e650
Vesa Kilpelänaho <vesa.kilpelanaho@op.fi>
2018-11-08 15:44:34 +0200

    initial

--- webviewtest/ViewController.swift
+++ webviewtest/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        testWKWebView()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func testWKWebView() {
+        let view = WKWebView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+        self.view.addSubview(view)
+        view.load(URLRequest(url: URL(string: "https://solo1.nordea.fi/")!))
+        
+    }
+    
+    func testUIWebView() {
+        let view = UIWebView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+        self.view.addSubview(view)
+        let url = URL(string: "https://tupas.nordea.fi/")!
+        let urelreq = URLRequest(url: url)
+        //        view.load(URLRequest(url: ))
+        view.loadRequest(urelreq)
+    }
 
 }
 
