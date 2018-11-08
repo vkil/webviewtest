fa95407207692e68c85310c26719167a869b30ff
Vesa Kilpelänaho <vesa.kilpelanaho@op.fi>
2018-11-08 15:33:30 +0200

    Initial Commit

---
+++ webviewtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webviewtest
+//
+//  Created by Vesa Kilpelänaho on 08/11/2018.
+//  Copyright © 2018 Vesa Oy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
